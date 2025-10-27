8839b1ddd76c0da1ab4ecff86dc4a158f4f36fbe
jungrim choi <jrimchoi@naver.com>
2025-10-27 17:44:52 +0900

    Remove .next directory from git tracking

--- .next/static/chunks/app_(auth)_sign-up_page_tsx_4168a37d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_60a6627e._.js",
-  "static/chunks/node_modules_132b3619._.js"
-],
-    source: "dynamic"
-});
