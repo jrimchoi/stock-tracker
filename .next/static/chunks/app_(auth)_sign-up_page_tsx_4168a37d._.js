676e9ea3d6cbea5ce88f883241f9d26d9f066b08
jungrim choi <jrimchoi@naver.com>
2025-10-27 17:26:26 +0900

    error correction

---
+++ .next/static/chunks/app_(auth)_sign-up_page_tsx_4168a37d._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_60a6627e._.js",
+  "static/chunks/node_modules_132b3619._.js"
+],
+    source: "dynamic"
+});
--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -99,7 +99,7 @@ export const sendDailyNewsSummary = inngest.createFunction(
 
                     userNewsSummaries.push({ user, newsContent });
                 } catch (e) {
-                    console.error('Failed to summarize news for : ', user.email);
+                    console.error('Failed to summarize news for : ', user.email, e);
                     userNewsSummaries.push({ user, newsContent: null });
                 }
             }
